e1c87a4e5e1f97ddcfd52c80f18190213768d1a7
jBuly4 <ibuly4@gmail.com>
2021-06-23 22:40:02 +0300

    first attempt

--- observer_solution.py
+++ observer_solution.py
@@ -1,11 +1,37 @@
 from abc import ABC
 
-class ObservableEngine:
+class ObservableEngine(Engine):
+    def __init__(self):
+        self.__subscribers = set()
+
     def subscribe(self, new_user):
-        pass
+        self.__subscribers.add(new_user)
 
     def unsubscribe(self, user):
-        pass
+        self.__subscribers.remove(user)
 
     def notify(self, message):
+        for subscriber in self.__subscribers:
+            subscriber.update(message)
+
+
+class AbstractObserver(ABC):
+    @abstractmethod
+    def update(self, message):
         pass
+
+
+class ShortNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = set()
+
+    def update(self, achievment):
+        self.achievements.add(achivement)
+
+
+class FullNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievments = []
+
+    def update(self, achievment):
+        self.achievements.append(achivement)
